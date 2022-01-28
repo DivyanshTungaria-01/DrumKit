4252ebc28b71d8ec2f22e6c07e2be33d34caada8
DivyanshTungaria-01 <divyanshtungaria@gmail.com>
2022-01-29 02:40:25 +0530

    Added background images

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for(var i = 0; i < numberOfDrumButtons; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-        alert("Button clicked !");
+        
+        // document.querySelectorAll(".drum").style.color = "white";
+        // this.style.color = "white";
+
     });
 
 }
 
 
-
+// var audio = new Audio('sounds/tom-1.mp3');
+//         audio.play();
