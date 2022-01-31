c08fb18574543a6f523e8f7c60963b5b5c7a0741
DivyanshTungaria-01 <divyanshtungaria@gmail.com>
2022-01-31 21:38:13 +0530

    Animation in process

--- index.js
+++ index.js
@@ -62,6 +62,11 @@ function buttonAnimation(currentKey){
 
     var activeButton = document.querySelector("." + currentKey);
     activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    },100); 
+
 }
 
 
